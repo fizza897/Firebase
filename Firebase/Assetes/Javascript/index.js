71469d8d6b833e335185ff1e633dba5d36c33ce0
fizza897 <99113832+fizza897@users.noreply.github.com>
2022-07-26 15:53:37 +0500

    Add files via upload

---
+++ Firebase/Assetes/Javascript/index.js
@@ -0,0 +1,31 @@
+
+//  <script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-app.js"></script>;
+//  <script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-auth.js"></script>;
+//  <script src="https://www.gstatic.com/firebasejs/7.15.5/firebase-database.js"></script>;
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyD_fn7NzhmBVisJCwcGugBjizwVH2n_TWI",
+//      authDomain: "firstweb-6b397.firebaseapp.com",
+//      databaseURL: "https://firstweb-6b397-default-rtdb.firebaseio.com",
+//     projectId: "firstweb-6b397",
+//      storageBucket: "firstweb-6b397.appspot.com",
+//     messagingSenderId: "710589099125",
+//      appId: "1:710589099125:web:fd8afb5c04c18c9bc75c3c"
+//    };
+
+//  // Initialize Firebase
+//  firebase.initializeApp(firebaseConfig); 
+
+// var rolls,names, email;
+// function Fetchdata() {
+//     rolls=document.getElementById('Roll').value
+//     names=document.getElementById("Name").value
+//     email=document.getElementById("Email").value
+// }
+// document.getElementById('insert').onclick=function(){ Fetchdata()
+//   firebase.database().ref('student/'+ rolls).set({
+//    RollNumber : rolls,
+//    Names:names,
+//    Email:email
+//   });
+
+// }
